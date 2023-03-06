9397f231b0f2cbb37209c96fe8491308e9f12600
Sam <sammynarduzzi@gmail.com>
2023-03-06 15:36:25 -0800

    create Question class

--- question.rb
+++ question.rb
@@ -1,3 +1,19 @@
 class Question 
+  def initialize(num1, num2)
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+  end
 
+  def question 
+    "What is #{@num1} + #{@num2}?"
+  end
+
+  def answer_correct?(answer)
+    answer = @num1 + @num2
+  end
+  
 end
+
+# checked if string outputs correctly with two random #'s
+# q = Question.new(@num1, @num2)
+# puts q.question
