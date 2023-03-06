2e56dfa838fa9dc9535fbcea40557d84b4a0e3d5
Sam <sammynarduzzi@gmail.com>
2023-03-06 15:22:16 -0800

    create Player class

--- player.rb
+++ player.rb
@@ -1,3 +1,28 @@
 class Player
+  def initialize(name)
+    @name = name
+    @lives = 3
+    @score = 0
+  end
+
+  def name
+    @name
+  end
+
+  def lives
+    @lives
+  end
+
+  def score
+    @score
+  end
+
+  def lose_life
+    @lives -= 1
+  end
+
+  def add_point
+    @score += 1
+  end
 
 end
