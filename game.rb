23fab455affca933ed8e1dce819e8a5946c19c76
Sam <sammynarduzzi@gmail.com>
2023-03-06 17:20:07 -0800

    create Game class and set up main.rb

--- game.rb
+++ game.rb
@@ -1,3 +1,54 @@
 class Game
-  
+  def initialize(player1, player2)
+    @players = [player1, player2]
+    @current_player = 0
+  end
+
+  def start_game
+    puts "----- NEW GAME -----"
+    until game_over?
+      new_turn
+      display_score
+    end
+    end_game
+  end
+
+  def new_turn
+    question = Question.new
+    current_player = @players[@current_player]
+    puts "#{current_player.name}: #{question.question}"
+    answer = gets.chomp.to_i
+    if answer == question.answer
+      puts "#{current_player.name}: YES! You are correct."
+    else
+      puts "#{current_player.name}: Seriously? No!"
+      current_player.lose_life
+    end
+    switch_player
+  end
+
+  def switch_player
+    @current_player = (@current_player + 1) % 2
+  end
+
+  def display_score
+    player1 = @players[0]
+    player2 = @players[1]
+    puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+    puts "----- NEW TURN -----"
+  end
+
+  def game_over?
+    @players.any? { |player| player.lives == 0 }
+  end
+
+  def end_game
+    winner = @players.max_by { |player| player.lives }
+    loser = @players.min_by { |player| player.lives }
+    puts "#{winner.name} wins with a score of #{winner.lives}/3"
+    puts "GAME OVER"
+    puts "Good bye!"
+  end
 end
+
+
--- main.rb
+++ main.rb
@@ -1,3 +1,14 @@
 require './player'
 require './game'
 require './question'
+
+puts "Welcome to the Math Game!"
+
+print "Player 1, enter your name: "
+player1 = Player.new(gets.chomp)
+
+print "Player 2, enter your name: "
+player2 = Player.new(gets.chomp)
+
+game = Game.new(player1, player2)
+game.start_game()
