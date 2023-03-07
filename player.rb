4d8f59cef1e55a4e6c3bb79c4b71293ad7d48509
Sam <sammynarduzzi@gmail.com>
2023-03-06 16:57:56 -0800

    refactored Player class

--- player.rb
+++ player.rb
@@ -1,28 +1,14 @@
 class Player
+  attr_reader :name, :lives
+
   def initialize(name)
     @name = name
-    @lives = 3
     @score = 0
-  end
-
-  def name
-    @name
-  end
-
-  def lives
-    @lives
-  end
-
-  def score
-    @score
+    @lives = 3
   end
 
   def lose_life
     @lives -= 1
-  end
-
-  def add_point
-    @score += 1
   end
 
 end
