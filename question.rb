5b4f73759e521f6051291907f93dbdf336f117c2
Sam <sammynarduzzi@gmail.com>
2023-03-06 17:04:48 -0800

    refactored Question class

--- question.rb
+++ question.rb
@@ -1,19 +1,14 @@
-class Question 
-  def initialize(num1, num2)
-    @num1 = rand(1..20)
-    @num2 = rand(1..20)
+class Question
+  attr_reader :question, :answer
+
+  def initialize
+    num1 = rand(1..20)
+    num2 = rand(1..20)
+    @question = "What is #{num1} + #{num2}?"
+    @answer = num1 + num2
   end
 
-  def question 
-    "What is #{@num1} + #{@num2}?"
+  def answer_correct?(input)
+    input == @answer
   end
-
-  def answer_correct?(answer)
-    answer = @num1 + @num2
-  end
-  
 end
-
-# checked if string outputs correctly with two random #'s
-# q = Question.new(@num1, @num2)
-# puts q.question
